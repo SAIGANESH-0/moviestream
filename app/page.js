afe041cf5a1100755d074c6820af1183ef2ee596
Saiganesh Voona <127197826+SAIGANESH-0@users.noreply.github.com>
2023-07-09 00:38:08 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,29 +1,26 @@
-"use client";
+
 import MovieList from "../app/components/MovieList";
 import axios from "axios";
-import { useState, useEffect } from "react";
 
-export default function Home() {
-  const [popularMovies, setMovies] = useState([]);
 
-  useEffect(() => {
-    // Fetch movie data from the API
-    const fetchData = async () => {
+export default async function Home() {
+  
+
+  
       try {
         const apiKey = "312af6ca07e7e14a45841dcaa68aa16e";
         const res = await axios.get(
           `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
         );
-        setMovies(res.data.results);
+        var popularMovies = res.data.results;
       } catch (error) {
         console.log(error);
       }
-    };
+    
 
-    fetchData();
-  }, []);
+ 
   return (
-    <div className="popular-movies bg-zinc-900">
+    <div className=" bg-black">
       <MovieList movie={popularMovies} />
     </div>
   );
