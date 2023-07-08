ab389772de0861f228d59e3b3eeb5f7c89ca8862
Saiganesh Voona <127197826+SAIGANESH-0@users.noreply.github.com>
2023-07-09 00:40:40 +0530

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,29 +3,18 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="bg-gray-900 text-white py-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-xl ml-2 font-bold">
-          Movies App
+      <div className="container mx-auto flex justify-center gap-5 items-center">
+        <Link href="/" className="text-xl  font-bold">
+          Movies
         </Link>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="/" className="hover:text-gray-300">
-                Movies
-              </Link>
-            </li>
-            {/* <li>
-              <Link href="/series" className="hover:text-gray-300">
-                Series
-              </Link>
-            </li> */}
-            <li>
-              <Link href="/search" className="hover:text-gray-300 mr-2">
+       
+             
+           
+              <Link href="/search" className=" text-xl  font-bold hover:text-gray-300 ">
                 Search
               </Link>
-            </li>
-          </ul>
-        </nav>
+           
+        
       </div>
     </header>
   );
